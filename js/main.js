165d39f55bb0f658ad7b63589238dcb7a6998291
jin <juannapo2003@gmail.com>
2024-03-18 20:07:04 -0300

    primer commit ruleta de casino

---
+++ js/main.js
@@ -0,0 +1,137 @@
+// Un número aleatorio entre 1 y 36
+const numeroAleatorio = Math.floor(Math.random() * 36) + 1;
+console.log(numeroAleatorio);
+
+// if (numeroAleatorio == 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36) {
+//     alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+
+// }
+
+
+//Resultado de la ruleta
+
+function color(rojo, negro) {
+
+    let diaDeLaSemana = numeroAleatorio;
+
+    switch (diaDeLaSemana) {
+        case 1:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 2:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 3:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 4:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 5:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 6:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 7:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 8:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 9:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 10:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 11:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 12:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 13:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 14:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 15:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 16:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 17:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 18:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 19:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 20:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 21:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 22:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 23:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 24:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 25:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 26:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 27:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 28:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 29:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 30:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 31:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 32:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 33:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 34:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+        case 35:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + negro);
+            break;
+        case 36:
+            alert("El numero que salio en la Ruleta es: " + numeroAleatorio + rojo);
+            break;
+    }
+
+
+}
+
+color(" Rojo 🟥🟥", " Negro ⬜⬜");
+
+
+
+
+
+
